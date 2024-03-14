e72b4d1cb5402fa806623bf464771bfbe34e84c9
vasco <vasco.schiavo@protonmail.com>
2024-03-15 00:49:54 +0100

    uast module

---
+++ uast/__init__.py
@@ -0,0 +1,10 @@
+from uast.cli.cli import execute_command_line_interface
+from uast.core.parser import parse
+
+
+def main() -> None:
+    execute_command_line_interface()
+
+
+if __name__ == "__main__":
+    main()
